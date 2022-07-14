6b97e4cffc39dd6ecfdee4d7423105f3e942f0a2
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2022-07-14 12:55:11 +0100

    Updated ninja_thief.cpp

--- picosystem/src/ninja_thief.cpp
+++ picosystem/src/ninja_thief.cpp
@@ -31,7 +31,7 @@ void init() {
 void update(uint32_t tick) {
     // Calculate change in time (in seconds) since last frame
     //float dt = (time() - last_time) / 1000.0f;
-    float dt = 0.01f;
+    float dt = 0.05f;
 
     // Cap dt (in case the game is paused/lagging)
     if (dt > 0.05f) {
